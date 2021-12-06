7ba346a6b86eb301276916b9399cad6eb90169f6
Oktay Nematov <oktayoki123@gmail.com>
2021-12-06 23:23:49 +0500

    saodsak

--- searchBox.js
+++ searchBox.js
@@ -2,12 +2,11 @@
 let searchBox = document.querySelector('.header__search-box');
 let searchBoxBtn = document.querySelector('.header__btn-search');
 searchBoxBtn.addEventListener('click', function () {
-  /* searchBoxBtn.classList.add('isClicked'); */
   searchBox.classList.remove('isClicked');
 });
 
 let closeSearchBox = document.querySelector('.search-box__closed-search');
-closeSearchBox.addEventListener('click', function() {
+closeSearchBox.addEventListener('click', function () {
   searchBox.classList.add('isClicked');
   searchBoxBtn.classList.remove('isClicked');
 });
